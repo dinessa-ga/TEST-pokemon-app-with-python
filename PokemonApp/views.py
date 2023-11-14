58e1f8e0f4e60412a356ddf2101179a626fe9de6
dinessa-ga <dinessa.ga@gmail.com>
2023-11-13 20:51:25 -0500

    error: HTTP Error 404: Not Found - solo muestra la interfaz, no trae datos al buscar

--- PokemonApp/views.py
+++ PokemonApp/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+import urllib.request
+import json
 
 # Create your views here.
+
+def index (request):
+    if request.method == 'POST':
+        pokemon = request.POST['pokemon'].lower()
+        pokemon = pokemon.replace('', '%20')
+        url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokemon/{pokemon}')
+        url_pokeapi.add_header('User-Agent','charmender')
+
+        source = urllib.request.urlopen(url_pokeapi).read()
+        list_of_data = json.loads(source)
+
+        # Hacer diccionario para que pueda leer json
+
+        data = {
+            "number": str(list_of_data['id']),
+            "name": str(list_of_data['name'].capitalize()),
+            "height": str(list_of_data['height']),
+            "weight": str(list_of_data['weight']),
+            "sprite": str(list_of_data['sprite']['front_default']),
+        }
+
+        print(data)
+    else:
+        data={}
+        return render(request, 'main/index.html', data)
+
+
+    
